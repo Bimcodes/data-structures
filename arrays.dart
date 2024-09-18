8e3e20bca40c90f69f7fd3299a62efb2d0116fa1
BB <akinuliolaolami@gmail.com>
2024-09-18 17:25:52 +0100

    Working on arrays(searching element and adding element)

--- arrays.dart
+++ arrays.dart
@@ -1,4 +1,17 @@
-class Array{
+class Array {
+  addAllElement(List initialArray, List newArray) {
+    initialArray.addAll(newArray);
+    //arr.addAll([1, 3, 5, 6, 7, 8]);
+    initialArray.sort();
+    return initialArray;
+  }
 
-  //print('Hello World');
+ String searchElement(int value, List arr, {int? number}) {
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == value) {
+        return 'The value was found at index $i';
+      }
+    }
+    return 'The value $value is not found in the array $arr';
+  }
 }
--- main.dart
+++ main.dart
@@ -1,6 +1,12 @@
 import 'arrays.dart';
 
 void main() {
-  Array;
+  Array array = Array();
+  List<int> arr = [1, 3, 4];
+  List<int> arr2 = [7, 9, 8, 4, 5, 0];
+  print(array.addAllElement(arr, arr2));
+  print(arr2);
+  print(array.searchElement(5, arr2));
+
   print("Hello World!");
 }
